3fefa81be3f472c1e23fb6d3ee9bcd2f39b27c51
baxx1199 <joshuajulcobaut@gmail.com>
2024-05-14 13:29:45 -0500

    modification in home component

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardsComponent } from './cards/cards.component';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardsComponent,
+    HomeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
+  ],
+  exports:[
+    HomeComponent,
+    CardsComponent
   ]
 })
 export class ModulesModule { }
